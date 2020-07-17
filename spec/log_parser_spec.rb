a71b42ad350e67848197fb8e028f31920ee24f3c
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2020-07-17 10:00:03 +0100

    test parse log file

--- spec/log_parser_spec.rb
+++ spec/log_parser_spec.rb
@@ -26,4 +26,19 @@ describe LogParser do
       end
     end
   end
+
+  describe '#parsed_logs' do
+    context 'when the file is parsed' do
+      let(:log) { Tempfile.new(['webserver', '.log']) }
+      it 'stores the logs in a hash' do
+        log.write("/help_page/1 126.318.035.038\n/contact 184.123.665.067")
+        log.read
+        log_parser.parse(log)
+        expect(log_parser.parsed_logs).to include(
+          '/help_page/1' => ['126.318.035.038'],
+          '/contact' => ['184.123.665.067']
+        )
+      end
+    end
+  end
 end
