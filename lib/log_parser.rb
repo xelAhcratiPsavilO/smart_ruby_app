2bc9433f85a613e78f9dbba035928a01601d12f8
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2020-07-17 10:05:27 +0100

    parse log file

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -1,11 +1,25 @@
 require_relative 'custom_exceptions'
 
 class LogParser
+  attr_reader :parsed_logs
+
   def initialize(log)
+    @log = log
     handle(log)
+    @parsed_logs = Hash.new { |hash, key| hash[key] = [] }
+  end
+
+  def parse(log)
+    log_lines = File.readlines(log)
+    log_lines.each do |line|
+      endpoint, ip_address = line.split
+      parsed_logs[endpoint] << ip_address
+    end
   end
 
   private
+
+  attr_reader :log
 
   def handle(log)
     raise FileLocationError unless File.exist?(log)
